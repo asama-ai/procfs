864565639005518dbcd591c2bbd4b816d0989208
Anuliak21 <anulika@asama.ai>
2025-12-05 12:54:52 +0530

    removed fileexist check from aercounters

--- sysfs/pci_device_test.go
+++ sysfs/pci_device_test.go
@@ -150,6 +150,7 @@ func TestPciDevices(t *testing.T) {
 		t.Fatalf("unexpected PciDevices (-want +got):\n%s", diff)
 	}
 }
+
 func TestParseDeviceLocation(t *testing.T) {
 	got, err := parsePciDeviceLocation("0001:9b:0c.0")
 	if err != nil {
